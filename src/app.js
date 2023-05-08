e857578af37e85976d4edc08258d3a09d62247e5
lchollet <leo.chollet.2000@gmail.com>
2023-05-08 13:54:09 +0200

    ajout de la conclusion et séparation du code en méthodes

--- src/app.js
+++ src/app.js
@@ -1,81 +1,195 @@
+//method to create a graph taking 3 parameters (number of countries, commodity and Id of the div where the graph will be displayed)
 function addChart(numCountries, commodity, chartId) {
+    d3.csv("../Datas/dom_clean_data.csv").then(function(data) {
 
+      // filter data to only include specified commodity and dates between 2019-01-01 and 2020-07-01
+      let filteredData = data.filter(function(d) {
+        return (d.commodity === commodity) && (d.month >= "1/1/2019") && (d.month <= "7/1/2020");
+      });
+
+      // group data by country and create a new array
+      let groupedData = d3.group(filteredData, function(d) { return d.country; });
+
+      // create an array of objects with country and price difference
+      let priceDiffByCountry = [];
+      groupedData.forEach(function(data, country) {
+        let prices = data.map(function(d) { return d.price; });
+        let priceDiff = prices[prices.length-1] - prices[0];
+        priceDiffByCountry.push({ country: country, priceDiff: priceDiff });
+      });
+
+      // sort the top numCountries countries by price difference
+      let topCountries = priceDiffByCountry.sort(function(a,b) {
+        return d3.descending(a.priceDiff, b.priceDiff);
+      }).slice(0, numCountries);
+
+      // call of the function to create the graph
+      createBarChart(chartId, topCountries);
+    });
+}
+
+const createConclusion = () => {
   d3.csv("../Datas/dom_clean_data.csv").then(function(data) {
-
-    // filter data to only include specified commodity and dates between 2019-01-01 and 2020-07-01
+    // filter data to dates between 2019-01-01 and 2020-07-01
     let filteredData = data.filter(function(d) {
-      return (d.commodity === commodity) && (d.month >= "1/1/2019") && (d.month <= "7/1/2020");
+      return (d.month >= "1/1/2019") && (d.month <= "7/1/2020");
     });
 
-    // group data by country and create a new array
-    let groupedData = d3.group(filteredData, function(d) { return d.country; });
+    // group data by commodity and create a new array
+    let groupedData = d3.group(filteredData, function(d) { return d.commodity; });
 
-    // create an array of objects with country and price difference
-    let priceDiffByCountry = [];
-    groupedData.forEach(function(data, country) {
+    // create an array of objects with commodity, price difference
+    let priceDiffByCommodity = [];
+    groupedData.forEach(function(data, commodity) {
       let prices = data.map(function(d) { return d.price; });
       let priceDiff = prices[prices.length-1] - prices[0];
-      priceDiffByCountry.push({ country: country, priceDiff: priceDiff });
+      priceDiffByCommodity.push({ commodity: commodity, priceDiff: priceDiff });
     });
 
-    // sort the top numCountries countries by price difference
-    let topCountries = priceDiffByCountry.sort(function(a,b) {
+    // create a list of the top 5 commodities with price difference
+    let topCommodities = priceDiffByCommodity.sort(function(a,b) {
       return d3.descending(a.priceDiff, b.priceDiff);
-    }).slice(0, numCountries);
+    }).slice(0, 5);
 
-    // initialize chart dimensions
-    let svgWidth = 1200;
-    let svgHeight = 400;
-    let margin = { top: 20, right: 20, bottom: 20, left: 60 };
-    let chartWidth = svgWidth - margin.left - margin.right;
-    let chartHeight = svgHeight - margin.top - margin.bottom;
+    console.log(topCommodities);
 
-    // create x and y scales
-    let xScale = d3.scaleLinear()
-      .domain([0, 15])
-      .range([0, chartWidth]);
+  // initialize chart dimensions
+  let svgWidth = 1200;
+  let svgHeight = 400;
+  let margin = { top: 20, right: 20, bottom: 20, left: 60 };
+  let chartWidth = svgWidth - margin.left - margin.right;
+  let chartHeight = svgHeight - margin.top - margin.bottom;
 
-    let yScale = d3.scaleBand()
-      .domain(topCountries.map(function(d) { return d.country; }))
-      .range([0, chartHeight])
-      .paddingInner(0.1)
-      .paddingOuter(0.2);
+  // create x and y scales
+  let xScale = d3.scaleLinear()
+    .domain([0, 15])
+    .range([0, chartWidth]);
 
-    // create SVG and chart group
-    let svg = d3.select(`#${chartId}`).append("svg")
-      .attr("width", svgWidth)
-      .attr("height", svgHeight);
+  let yScale = d3.scaleBand()
+    .domain(topCommodities.map(function(d) { return d.commodity; }))
+    .range([0, chartHeight])
+    .paddingInner(0.1)
+    .paddingOuter(0.2);
+    
+  // create SVG and chart group
+  let svg = d3.select("#conclusion").append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
 
-    let chartGroup = svg.append("g")
-      .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  let chartGroup = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  // create bars and labels
+  let bars = chartGroup.selectAll("rect")
+    .data(topCommodities);
 
-    // create bars and labels
-    chartGroup.selectAll("rect")
-      .data(topCountries)
-      .enter()
-      .append("rect")
-      .attr("x", 0)
-      .attr("y", function(d) { return yScale(d.country); })
-      .attr("width", function(d) { return xScale(d.priceDiff); })
-      .attr("height", yScale.bandwidth())
-      .attr("fill", "steelblue");
+  //animate the bars
+  bars.enter()
+    .append("rect")
+    .merge(bars)
+    .transition()
+    .duration(2000) // duration of the transition in milliseconds
+    .attr("x", 0)
+    .attr("y", function(d) { return yScale(d.commodity); })
+    .attr("width", function(d) { return xScale(d.priceDiff); })
+    .attr("height", yScale.bandwidth())
+    .attr("fill", "steelblue");
 
-    chartGroup.selectAll("text")
-      .data(topCountries)
-      .enter()
-      .append("text")
-      .text(function(d) { return `${d.country} - ${d.priceDiff}%`; })
-      .attr("x", function(d) { return xScale(d.priceDiff) + 5; })
-      .attr("y", function(d) { return yScale(d.country) + yScale.bandwidth() / 2; })
-      .attr("dy", "0.35em");
+  bars.exit()
+    .remove();
 
-    //ajustement de la taille de la police
-    chartGroup.selectAll("text").style("font-size", "14px");
+  let labels = chartGroup.selectAll("text")
+    .data(topCommodities);
 
+  labels.enter()
+    .append("text")
+    .merge(labels)
+    .transition()
+    .duration(800) // duration of the transition in milliseconds
+    .text(function(d) { return `${d.commodity} - ${d.priceDiff}%`; })
+    .attr("x", function(d) { return xScale(d.priceDiff) + 5; })
+    .attr("y", function(d) { return yScale(d.commodity) + yScale.bandwidth() / 2; })
+    .attr("dy", "0.35em")
+    .style("font-size", "14px");
+
+  labels.exit()
+    .remove();
   });
 }
+
+
+//method to create a graph taking 2 parameters (Id of the div where the graph will be displayed and the data to display)
+const createBarChart = (chartId, dataToDislpay) => {
+
+  //console.log(dataToDislpay);
+  // initialize chart dimensions
+  let svgWidth = 1200;
+  let svgHeight = 400;
+  let margin = { top: 20, right: 20, bottom: 20, left: 60 };
+  let chartWidth = svgWidth - margin.left - margin.right;
+  let chartHeight = svgHeight - margin.top - margin.bottom;
+
+  // create x and y scales
+  let xScale = d3.scaleLinear()
+    .domain([0, 15])
+    .range([0, chartWidth]);
+
+  let yScale = d3.scaleBand()
+    .domain(dataToDislpay.map(function(d) { return d.country; }))
+    .range([0, chartHeight])
+    .paddingInner(0.1)
+    .paddingOuter(0.2);
+    
+  // create SVG and chart group
+  let svg = d3.select(`#${chartId}`).append("svg")
+    .attr("width", svgWidth)
+    .attr("height", svgHeight);
+
+  let chartGroup = svg.append("g")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
+  // create bars and labels
+  let bars = chartGroup.selectAll("rect")
+    .data(dataToDislpay);
+
+  //animate the bars
+  bars.enter()
+    .append("rect")
+    .merge(bars)
+    .transition()
+    .duration(2000) // duration of the transition in milliseconds
+    .attr("x", 0)
+    .attr("y", function(d) { return yScale(d.country); })
+    .attr("width", function(d) { return xScale(d.priceDiff); })
+    .attr("height", yScale.bandwidth())
+    .attr("fill", "steelblue");
+
+  bars.exit()
+    .remove();
+
+  let labels = chartGroup.selectAll("text")
+    .data(dataToDislpay);
+
+  labels.enter()
+    .append("text")
+    .merge(labels)
+    .transition()
+    .duration(800) // duration of the transition in milliseconds
+    .text(function(d) { return `${d.country} - ${d.priceDiff}%`; })
+    .attr("x", function(d) { return xScale(d.priceDiff) + 5; })
+    .attr("y", function(d) { return yScale(d.country) + yScale.bandwidth() / 2; })
+    .attr("dy", "0.35em")
+    .style("font-size", "14px");
+
+  labels.exit()
+    .remove();
+}
+
+
+
 addChart(5, "Beef meat", "beef");
 addChart(5, "Milk", "wheat");
 addChart(5, "Maize", "corn");
 addChart(5, "Sugar", "sugar");
 addChart(5, "Rice", "rice");
+createConclusion();
+
+
